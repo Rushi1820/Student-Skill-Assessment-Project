e9b4926f1e7bacc1d2fa1ec191d702b3e43e84fc
rushi <rushivardhan18@gmail.com>
2023-09-02 23:00:29 +0530

    added project file

---
+++ Student Skill/Student-Skill/src/main/java/Student/Skill/Assessment/Student/Skill/StudentSkillApplication.java
@@ -0,0 +1,13 @@
+package Student.Skill.Assessment.Student.Skill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSkillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSkillApplication.class, args);
+	}
+
+}
---
+++ Student Skill/Student-Skill/src/test/java/Student/Skill/Assessment/Student/Skill/StudentSkillApplicationTests.java
@@ -0,0 +1,13 @@
+package Student.Skill.Assessment.Student.Skill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSkillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
